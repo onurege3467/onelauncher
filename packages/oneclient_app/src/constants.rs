e2ceee2ce0d16203a10e90f003532dcdc74e85eb
onurege3467 <info@onurege.click>
2026-09-09 13:28:02 +0300

    chore: pencere basligini Fxes Launcher yap, Polyfrost/Mojang telifli gorselleri kaldir
    WINDOW_APP_ID/WINDOW_TITLE Fxes'e cevrildi. logo.svg (Polyfrost/OneLauncher
    markasi, all-rights-reserved) ve backgrounds/CavesAndCliffs.jpg (Mojang promo
    goruntusu) silindi; ikisi de kod tarafinda unwrap_or_default() ile guvenle
    bos donuyor, panic riski yok. Gorsel yerine koyma Asama 3'te.

--- packages/oneclient_app/src/constants.rs
+++ packages/oneclient_app/src/constants.rs
@@ -1,5 +1,5 @@
-pub const WINDOW_APP_ID: &str = "oneclient_app";
-pub const WINDOW_TITLE: &str = "OneClient";
+pub const WINDOW_APP_ID: &str = "fxes_launcher";
+pub const WINDOW_TITLE: &str = "Fxes Launcher";
 
 pub const UPDATER_PUBKEY: &str = "dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IDFGODk3MkMyMjg0MjFDMDUKUldRRkhFSW93bktKSHpkWjNEMXNzaDVINVpCTU8xSnhuK2RnV0dTZ2FkcFJWbG1zUkhGYTNjaUkK";
 pub const UPDATER_ENDPOINT: &str = "https://github.com/Polyfrost/OneLauncher/releases/latest/download/latest.json";
